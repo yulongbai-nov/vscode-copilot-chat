c0fe68f692817934998cb571effec8394a54ec2b
Yulong Bai <yulong.bai@nov.com>
2025-12-13 19:53:28 +0000

    chore(dev): run build/lint TS under Node 22
    Use node --experimental-strip-types so Node can execute .esbuild.ts and ESLint can load the local TS plugin (.eslintplugin/*.ts).

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,25 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const ESLint = require('eslint').ESLint;
-
-const removeIgnoredFiles = async (files) => {
-	const eslint = new ESLint();
-	const isIgnored = await Promise.all(
-		files.map((file) => {
-			return eslint.isPathIgnored(file);
-		})
-	);
-	const filteredFiles = files.filter((_, i) => !isIgnored[i]);
-	return filteredFiles.join(' ');
-};
-
 module.exports = {
 	'!({.esbuild.ts,test/simulation/fixtures/**,test/scenarios/**,.vscode/extensions/**,**/vscode.proposed.*})*{.ts,.js,.tsx}': async (files) => {
-		const filesToLint = await removeIgnoredFiles(files);
+		const filesToLint = files.join(' ');
 		return [
 			`npm run tsfmt -- ${filesToLint}`,
-			`eslint --max-warnings=0 ${filesToLint}`
+			`node --experimental-strip-types ./node_modules/eslint/bin/eslint.js --max-warnings=0 --no-warn-ignored ${filesToLint}`
 		];
 	},
 };
